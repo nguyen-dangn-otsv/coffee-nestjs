fa58e161aca5442f658311dc6fd189cf05979198
nguyen-dangn-otsv <nguyen.dangn@one-line.com>
2021-12-24 14:59:31 +0700

    dasdas

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  console.log('abc');
+  const a = 5;
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
